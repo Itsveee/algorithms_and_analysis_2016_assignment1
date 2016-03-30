56775204c243d362e8b06f9168a6e067de0b46f3
Loy Larvin <larvinloy@gmail.com>
2016-03-31 01:02:26 +1100

    Updated: Sortedlikedlist

--- src/SortedLinkedListMultiset.java
+++ src/SortedLinkedListMultiset.java
@@ -70,12 +70,13 @@ public class SortedLinkedListMultiset<T> extends Multiset<T>
 			Node<T> currNode = mHead;
 			do
 			{
-				if(mComparator.compare(currNode.getValue(), item) == 0)
-					return currNode.getCount();
-				currNode = currNode.getNext();
 				if(mComparator.compare(currNode.getValue(), item) > 0)
 					// The item does not exist because current item is > than item
 					break;
+				if(mComparator.compare(currNode.getValue(), item) == 0)
+					return currNode.getCount();
+				currNode = currNode.getNext();
+				
 			}
 			while(currNode != null);
 		}
@@ -110,11 +111,11 @@ public class SortedLinkedListMultiset<T> extends Multiset<T>
 						break;
 					}
 				}
-				prevNode = currNode;
-				currNode = currNode.getNext();
 				if(mComparator.compare(currNode.getValue(), item) > 0)
 					// The item does not exist because current item is > than item
 					break;
+				prevNode = currNode;
+				currNode = currNode.getNext();
 			}
 			while(currNode != null);
 		}
@@ -137,11 +138,11 @@ public class SortedLinkedListMultiset<T> extends Multiset<T>
 						prevNode.setNext(currNode.getNext());
 					break;
 				}
-				prevNode = currNode;
-				currNode = currNode.getNext();
 				if(mComparator.compare(currNode.getValue(), item) > 0)
 					// The item does not exist because current item is > than item
 					break;
+				prevNode = currNode;
+				currNode = currNode.getNext();
 			}
 			while(currNode != null);
 		}
